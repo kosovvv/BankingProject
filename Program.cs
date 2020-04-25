2010c72f1ce084607737f5d6b333817388cd5fa2
ASD ASD <emilkosov02@hotmail.com>
2020-04-25 18:46:46 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using BankProject;
+﻿using BankingProject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BankProject.Properties {
+namespace BankingProject.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BankProject.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BankProject.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BankingProject.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
